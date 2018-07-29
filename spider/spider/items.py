c7edbdaceb79f89533fa9b171c0bc46cb0749c79
wederribas <wederribas@gmail.com>
2018-07-29 11:33:54 -0300

    Refactor customer spider with Items loades / Add docstrings

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader.processors import MapCompose
+
+
+def strip_string(value):
+    """Remove all blank spaces from the ends of a given value."""
+    return value.strip()
 
 
 class SpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     pass
+
+
+class Report(scrapy.Item):
+
+    """Instantiate the customer reports items fields, with its processors.
+
+    Each field is an instance of scrapy.Field. Some fields will receive a
+    input processor in order to peform parsing operations over this field
+    before loading them into the item.
+
+    """
+
+    company_name = scrapy.Field(input_processor=MapCompose(strip_string))
+    user_report = scrapy.Field(input_processor=MapCompose(strip_string))
+    company_response = scrapy.Field(input_processor=MapCompose(strip_string))
+    status = scrapy.Field(input_processor=MapCompose(strip_string))
+    user_feedback = scrapy.Field(input_processor=MapCompose(strip_string))
+    user_rating = scrapy.Field(input_processor=MapCompose(strip_string))
+    date = scrapy.Field()
+    location = scrapy.Field()
--- spider/spider/spiders/customer_report.py
+++ spider/spider/spiders/customer_report.py
@@ -1,47 +1,94 @@
+# -*- coding: utf-8 -*-
+
 import scrapy
+from scrapy.loader import ItemLoader
+from spider.items import Report
 
 
 class CustomerReports(scrapy.Spider):
+
+    """Spider that retrieves raw customer reports about companies.
+
+    This spider reaches the consumidor.gov.br website to crawl the data from
+    the customer raw reports page. The specific page containing this information
+    is composed by a hidden form that is submitted to retrieve the reports.
+    For this reason, this spider reaches the initial page and then trigger the
+    form submission to obtain the data. The website returns 10 values per call.
+
+    The default Scrapy `parse` method will perform the form submission.
+
+    The `parse_response` method is a callback for `parse` and will crawl the
+    required data from the website response, loading the scrapy items.
+    """
+
     name = 'reports'
     start_urls = [
         'https://www.consumidor.gov.br/pages/indicador/relatos/abrir'
     ]
 
     def parse(self, response):
+        """Trigger the hidden form POST to get the customer report."""
+
         return scrapy.FormRequest(
             url="https://consumidor.gov.br/pages/indicador/relatos/consultar",
-            callback=self.after_fetch,
+            callback=self.parse_response,
             formdata={"indicePrimeiroResultado": "0", "palavrasChave": ""},
         )
 
-    def after_fetch(self, response):
-        for report in response.css('div.cartao-relato'):
+    def parse_response(self, response):
+        """Parses the response data, crawling the required fields and loading
+        the Item to populate the final data set."""
+
+        for crawled_report in response.css('div.cartao-relato'):
+            # Loader responsible to process input and populate an item
+            loader = ItemLoader(item=Report(), response=response)
+
             # Generate index from own element counter
-            index = report.css(
+            index = crawled_report.css(
                 'h5.relatos-contador::text').extract_first()
             index = int(index) - 1
 
-            company_name = report.css(
-                'h3.relatos-nome-empresa a::text').extract_first()
-            users_report = report.xpath(
-                '//div[strong="Relato"]/p/text()').extract()
-            user_report = users_report[index]
-            report_status = report.css(
-                'h4.relatos-status::text').extract_first()
+            loader.add_value(
+                'company_name',
+                crawled_report.css(
+                    'h3.relatos-nome-empresa a::text').extract_first()
+            )
 
-            user_date_location = report.xpath(
+            users_report = crawled_report.xpath(
+                '//div[strong="Relato"]/p/text()').extract()
+            loader.add_value('user_report', users_report[index])
+
+            company_response = crawled_report.xpath(
+                '//div[strong="Resposta"]/p/text()').extract()
+            loader.add_value(
+                'company_response',
+                company_response[index]
+            )
+
+            loader.add_value(
+                'status',
+                crawled_report.xpath(
+                    '//h4[@class="relatos-status"]/text()').extract_first()
+            )
+
+            users_feedback = crawled_report.xpath(
+                '//div[strong="Avaliação"]/p[2]/text()').extract()
+            loader.add_value('user_feedback', users_feedback[index])
+
+            users_rating = crawled_report.xpath(
+                '//div[strong="Avaliação"]/p[1]/text()').extract()
+            loader.add_value('user_rating', users_rating[index])
+
+            user_date_location = crawled_report.xpath(
                 '//div[strong="Relato"]'
                 '/span[descendant::i[@class="glyphicon glyphicon-calendar"]]')
-            user_date_location = user_date_location.css(
-                'span::text').extract()
+            user_date_location = user_date_location.css('span::text').extract()
             user_date_location = [
                 item for item in user_date_location if item != ' ']
-            date, location = user_date_location[index].split(',')
 
-            yield {
-                "companyName": company_name.strip(),
-                "userReport": user_report.strip(),
-                "status": report_status.strip(),
-                "date": date,
-                "location": location
-            }
+            # Date and location are stored in the same element. Need to split.
+            date, location = user_date_location[index].split(',')
+            loader.add_value('date', date)
+            loader.add_value('location', location)
+
+            yield loader.load_item()
